ca1e434ce73343d0142c707f31c65967ee073167
Vodnala Srujana <146533051+VodnalaSrujana004@users.noreply.github.com>
2024-11-10 07:20:43 +0530

    Delete Project/store-client directory

--- Project/store-client/src/components/Admin/AdminTopBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminTopBar = () => {
-  return (
-    <div className='w-full h-full bg-green-500/20 flex justify-center items-center'>AdminTopBar</div>
-  )
-}
-
-export default AdminTopBar
